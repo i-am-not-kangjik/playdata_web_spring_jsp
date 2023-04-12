3d74c0df3a6e183e552d7074e4e862da678032d8
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 12:04:04 +0900

    UserSecurityService를 등록

--- kjkim-spring/src/main/java/kjkim/kjkimspring/SecurityConfig.java
+++ kjkim-spring/src/main/java/kjkim/kjkimspring/SecurityConfig.java
@@ -2,6 +2,8 @@ package kjkim.kjkimspring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,4 +31,10 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean // UserSecurityService 등록
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
 }
