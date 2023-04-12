f0a779d0d1d204bea71ac4f82253084a8573d486
Kangjik Kim <rkqqkb@gmail.com>
2023-04-12 12:29:20 +0900

    로그아웃 페이ㅈㅣ 추가

--- kjkim-spring/src/main/java/kjkim/kjkimspring/SecurityConfig.java
+++ kjkim-spring/src/main/java/kjkim/kjkimspring/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration  // 스프링의 환결설정 파일임을 의미하는 애너테이션
                 // 스프링 시큐리티의 설정을 위해 사용
@@ -22,8 +23,9 @@ public class SecurityConfig {
                 .and().csrf().ignoringAntMatchers("/h2-console/**")
                 .and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
                         XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN  // SAMEORIGIN을 설정하면 frame에 포함된 페이지가 페이지를 제공하는 사이트와 동일한 경우 계속 사용할 수 있다.
-                )).and().formLogin().loginPage("/user/login").defaultSuccessUrl("/"); // 스프링 시큐리티 로그인 설정으로 로그인 URL 경로는 /user/login, login 성공하면 이동하는 페이지는 루트로 지정
-
+                )).and().formLogin().loginPage("/user/login").defaultSuccessUrl("/")// 스프링 시큐리티 로그인 설정으로 로그인 URL 경로는 /user/login, login 성공하면 이동하는 페이지는 루트로 지정
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/").invalidateHttpSession(true); //로그아웃 기능
         return http.build();
     }
 
